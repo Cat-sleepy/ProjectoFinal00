616daa59141e870286a9abcd9c053091b90ce8b3
Cat-sleepy <caty.vizela@gmail.com>
2026-03-19 03:30:50 +0000

    asneira

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+// src/environments/environment.development.ts
+
+export const environment = {
+  production: false,
+  supabase: {
+    url: 'https://sidtlgxguovlfovmxjku.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpZHRsZ3hndW92bGZvdm14amt1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4MzUyMTgsImV4cCI6MjA4OTQxMTIxOH0.moXSKz5NRDeZp_iQgsO5PWU8tN493r9aKdfBTgINiYc'
+  }
+};
+
